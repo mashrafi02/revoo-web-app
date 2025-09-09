aafd69c55a86afe81f76aaba8e96aa370998f588
mashrafi <ahmedfirojmash365@gmail.com>
2025-09-09 16:10:12 +0600

    small tweaks done

--- src/Components/NavBarPrivateProf.jsx
+++ src/Components/NavBarPrivateProf.jsx
@@ -44,7 +44,7 @@ function NavbarPrivateProf() {
     <nav className="w-full bg-transparent absolute z-50">
       <div className="max-w-7xl mx-auto px-6 py-4 flex justify-between items-center">
         {/* Logo */}
-        <Link to="/" className="text-2xl font-bold text-white">
+        <Link to="/">
           <img src="/images/Revoo.png" alt="app-logo" className="w-[100px]" />
         </Link>
 
@@ -53,13 +53,13 @@ function NavbarPrivateProf() {
                 <div className="flex space-x-4">
                     <Link
                         to="/login"
-                        className="px-4 py-2 border font-semibold border-white text-white rounded-lg hover:bg-red-400 hover:text-white duration-300"
+                        className="px-2 md:px-4 py-2 text-sm md:text-base border font-semibold border-white text-white rounded-lg hover:bg-red-400 hover:text-white duration-300"
                     >
                         Log In
                     </Link>
                     <Link
                         to="/signup"
-                        className="px-4 py-2 bg-red-400 font-semibold text-white rounded-lg hover:bg-gray-700 duration-300"
+                        className="px-2 md:px-4 py-2 text-sm md:text-base bg-red-400 font-semibold text-white rounded-lg hover:bg-gray-700 duration-300"
                     >
                         Sign Up
                     </Link>
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -40,7 +40,7 @@ function Navbar() {
     <nav className="w-full bg-transparent absolute z-50">
       <div className="max-w-7xl mx-auto px-6 py-4 flex justify-between items-center">
         {/* Logo */}
-        <Link to="/" className="text-2xl font-bold text-white">
+        <Link to="/">
           <img src="/images/Revoo.png" alt="app-logo" className="w-[100px]" />
         </Link>
 
@@ -75,16 +75,16 @@ function Navbar() {
         
         {
             user === null ? (
-                <div className="flex space-x-4">
+                <div className="flex space-x-2 md:space-x-4">
                     <Link
                         to="/login"
-                        className="px-4 py-2 border font-semibold border-white text-white rounded-lg hover:bg-red-400 hover:text-white duration-300"
+                        className="px-2 text-sm md:text-base md:px-4 py-2 border font-semibold border-white text-white rounded-lg hover:bg-red-400 hover:text-white duration-300"
                     >
                         Log In
                     </Link>
                     <Link
                         to="/signup"
-                        className="px-4 py-2 bg-red-400 font-semibold text-white rounded-lg hover:bg-gray-700 duration-300"
+                        className="px-2 text-sm md:text-base md:px-4 py-2 bg-red-400 font-semibold text-white rounded-lg hover:bg-gray-700 duration-300"
                     >
                         Sign Up
                     </Link>
